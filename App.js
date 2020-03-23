e5d1c769ef45bd6d56003bf19a2f53b620cecc1c
Víctor Peña Romero <vic.pero@icloud.com>
2020-03-23 13:53:56 -0600

    Alerts

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native'
 import Header from './components/header'
 import TodoItem from './components/todoitem';
 import AddTodo from './components/addtodo';
@@ -19,12 +19,20 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        {text: text, key: Math.random().toString() },
-        ...prevTodos
-      ]
-    })
+
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          {text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOPS!', 'Todos must be over 3 characters long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}    
+      ])
+    }
+
   }
 
   return (
