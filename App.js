043a797d8c90071928ef5425a592c1920f13bf26
Víctor Peña Romero <vic.pero@icloud.com>
2020-03-23 12:52:37 -0600

    Todo App (part 2)

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native'
 import Header from './components/header'
+import TodoItem from './components/todoitem';
 
 export default function App() {
 
@@ -8,7 +9,13 @@ export default function App() {
     { text: 'buy cofee', key: '1' },
     { text: 'create an app', key: '2' },
     { text: 'play on the switch', key: '3' }
-  ])  
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
 
   return (
     <View style={styles.container}>
@@ -19,7 +26,7 @@ export default function App() {
           <FlatList 
             data={todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler}/>
             )}
           />
         </View>
