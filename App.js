f477cf73eb8f3f803f4c41feb9ab207cd91fa99c
Víctor Peña Romero <vic.pero@icloud.com>
2020-03-23 12:22:41 -0600

    Todo App (part 1)

--- App.js
+++ App.js
@@ -1,47 +1,29 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import Header from './components/header'
 
 export default function App() {
 
-  const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
-  ])
-
-  const pressHandler = id => {
-    console.log(id)
-    setPeople(prevPeople => {
-      return prevPeople.filter(person => person.id != id)
-    })
-  }
+  const [todos, setTodos] = useState([
+    { text: 'buy cofee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ])  
 
   return (
     <View style={styles.container}>
-
-      <FlatList 
-        numColumns={2}
-        keyExtractor={item => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      
-      {/* <ScrollView>
-        { people.map(item => (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-        ))}
-      </ScrollView> */}
-
+      <Header />
+      <View style={styles.content}>
+        {/* to-do form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -49,18 +31,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    // alignItems: 'center',
-    //justifyContent: 'center',
+    backgroundColor: '#fff'
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
   }
 })
